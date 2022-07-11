34087e5b59fab46358f3b1c066ccf9e5ce397cf4
Tee <teslimb36@gmail.com>
2022-07-11 12:29:07 -0700

    Create functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - main entry.
+ * @c: Integer to be checked.
+ * Return: 1 if c is uppercase, 0 otherwise.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiply two given integers
+ * @a: First given integer
+ * @b: second given integer
+ *
+ * Return: Return product of two integers
+ */
+
+int mul(int a, int b)
+{
+	return a * b;
+}
