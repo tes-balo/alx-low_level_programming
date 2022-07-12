5aea346a03a13c43a7a837a6616cd0f69d2acfec
Tee <teslimb36@gmail.com>
2022-07-12 14:04:02 -0700

    hotfix

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,32 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
+
 /**
- * main - generates keygen.
- * Return: 0 Always.
+ * main - paso
+ *
+ * Return: int 0;
  */
 int main(void)
 {
-	int r = 0, c = 0;
-	time_t t;
+	char str[100];
+	int i = 0, randNum = 0, suma = 0;
 
-	srand((unsigned int) time(&t));
-	while (c < 2772)
+	srand (time(NULL));
+
+
+	for (i = 0; suma <= 2644; i++)
 	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
-			break;
-		c = c + r;
-		printf("%c", r);
+		randNum = (rand() % 25) + 65;
+
+		str[i] = randNum;
+		suma = suma + randNum;
 	}
-	printf("%c\n", (2772 - c));
+
+	str[i++] = 2772 - suma;
+	str[i++] = '\0';
+
+	printf("%s\n", str);
+
 	return (0);
 }
